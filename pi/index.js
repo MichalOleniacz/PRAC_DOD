9dd57d9a48a71d958e87c81f9ca15883aa186190
MichalOleniacz <oleniaczmichal852@gmail.com>
2020-11-29 17:38:53 +0100

    feat(pi): added resource logging fuctionality to Raspberry Pi
    Raspberry Pi software now logs all OS and hardware related data and posts it to the db

---
+++ pi/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const si = require("systeminformation");
+const assert = require("assert");
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const url = `mongodb+srv://${process.env.USER}:${process.env.PASSWD}@cluster0.e6cry.mongodb.net/PiUsageInfo?retryWrites=true&w=majority`;
+
+const dbName = process.env.DB_NAME;
+const collectionName = process.env.COLLECTION_NAME;
+
+const app = express();
+
+const PORT = 5000;
+
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
+  assert.equal(null, err);
+  app.listen(PORT);
+
+  const db = client.db(dbName);
+
+  const collection = db.collection(collectionName);
+
+  const getUsageInfo = async () => await si.getAllData();
+
+  const insertNewData = async () => {
+    getUsageInfo().then((res) => {
+      db.collection(collectionName).insertOne(res);
+    });
+  };
+
+  insertNewData();
+});

7784bccd7369a9e295025857231ca8aca3471bd6
MichalOleniacz <oleniaczmichal852@gmail.com>
2020-12-12 17:11:09 +0100

    feat(pi): removed unused code

--- pi/eventPipeline.js
+++ pi/eventPipeline.js
@@ -3,31 +3,6 @@ const getGeneralUsageInfo = require("./middleware/getGeneralUsageInfo");
 const validateBody = require("./middleware/validateBody");
 const postNewData = require("./middleware/postNewData");
 
-// class Pipeline {
-//   constructor(...args) {
-//     this.pipe = args;
-//     this.iterator = 0;
-//     this.next = (...args) => {
-//       if (this.iterator + 1 < this.pipe.length) {
-//         this.iterator = this.iterator + 1;
-//         try {
-//           let next = this.pipe[this.iterator];
-//           console.log(typeof next);
-//           next.call([...args], (...args) => this.next(...args));
-//         } catch (error) {
-//           console.log(error);
-//         }
-//       } else {
-//         this.iterator = 0;
-//       }
-//     };
-//     this.start = () => {
-//       this.iterator = 0;
-//       this.pipe[0]((...args) => this.next(...args));
-//     };
-//   }
-// }
-
 class LinkedList {
   constructor(head = null) {
     this.head = head;
@@ -58,8 +33,6 @@ class Node {
   }
 }
 
-const common = [validateBody];
-
 const detailedList = new LinkedList();
 detailedList.addNode(getDetailedUsageInfo);
 detailedList.addNode(validateBody);
@@ -69,7 +42,5 @@ const generalList = new LinkedList();
 generalList.addNode(getGeneralUsageInfo);
 generalList.addNode(validateBody);
 generalList.addNode(postNewData);
-
-detailedList.run();
 
 module.exports = [detailedList, generalList];
